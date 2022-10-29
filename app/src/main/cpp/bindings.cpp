ffb4858a527f45cc411387863b793b711bce2695
vladd11 <rozhkov.2006@gmail.com>
2022-10-29 02:38:11 -0700

    Fixed line renderer. Now Lua STL will be available on script execution or code execution (trough Websocket). Added first real-use binding.

---
+++ app/src/main/cpp/bindings.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vladislav on 10/28/22.
+//
+
+#include "bindings.h"
---
+++ app/src/main/cpp/bindings.h
@@ -0,0 +1,8 @@
+//
+// Created by vladislav on 10/28/22.
+//
+
+#ifndef AR_SHOP_BINDINGS_H
+#define AR_SHOP_BINDINGS_H
+
+#endif //AR_SHOP_BINDINGS_H
