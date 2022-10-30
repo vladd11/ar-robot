0bf2b38eaf6c675f8127f1272251fbed501e9dc3
vladd11 <rozhkov.2006@gmail.com>
2022-10-30 05:49:30 -0700

    Added permission request.

--- app/src/main/java/com/vladd11/arshop/MainActivity.kt
+++ app/src/main/java/com/vladd11/arshop/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.vladd11.arshop
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import java.util.concurrent.locks.ReentrantLock
 
 class MainActivity : AppCompatActivity() {
-    lateinit var glSurfaceView: ArSurfaceView
-    lateinit var navController: NavController
+    private lateinit var glSurfaceView: ArSurfaceView
+    private lateinit var navController: NavController
+
+    companion object {
+        const val REQUEST_CODE = 0;
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,20 +32,29 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFragment.navController.also {
             it.navigate(R.id.dummyFragment)
         }
-
-        requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 0)
     }
 
     override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
     ) {
+        if (requestCode == REQUEST_CODE) {
+            if(grantResults.isEmpty() || grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                Toast.makeText(this, "Please grant CAMERA permission", Toast.LENGTH_LONG).show()
+                finish()
+            }
+        }
+
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     override fun onResume() {
         super.onResume()
+
+        if(checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 0)
+            return
+        }
+
         glSurfaceView.resume(this)
     }
 
@@ -55,8 +70,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun hideSystemBars() {
         WindowCompat.getInsetsController(window, window.decorView).apply {
-            systemBarsBehavior =
-                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
             hide(WindowInsetsCompat.Type.systemBars())
         }
     }
